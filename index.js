c93c9d77b7ab93f8940d4b4dce494e6e8c20544a
威老 <qqq123026689@126.com>
2015-02-12 15:42:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var crypto = require('crypto'),
+    path = require('path'),
+    fs = require('fs');
+
+var moment = require('moment');
+
+/**
+ * Check if cache available
+ * @param cachedFilePath
+ * @param expire
+ * @returns {boolean}
+ */
+function isCacheAvailable(cachedFilePath, expire) {
+    var cacheModifiedTime, arr;
+
+    if (!fs.existsSync(cachedFilePath)) return false;
+
+    cacheModifiedTime = moment(fs.statSync(cachedFilePath).mtime);
+
+    arr = expire.split(' ');
+    return cacheModifiedTime.add(arr[0], arr[1]) > moment();
+}
+
+module.exports = function (connection, opts) {
+    var realQuery, expire, cacheDir;
+
+    expire = opts.expire || '1 hour';
+    cacheDir = opts.cache_dir || '/cache';
+
+    realQuery = connection.query;
+    connection.query = function (sql, values, callback) {
+        var key, cacheFileName, cacheFilePath, result;
+
+        key = sql + JSON.stringify(values);
+        cacheFileName = crypto.createHash('sha1').update(key).digest('hex');
+        cacheFilePath = path.join(cacheDir, cacheFileName);
+
+        if (isCacheAvailable(cacheFilePath, expire)) {
+            // hit
+            result = fs.readFileSync(cacheFilePath);
+            callback(null, JSON.parse(result));
+
+        } else {
+            // no hit
+
+            // execute the real query
+            return realQuery.apply(connection, [sql, values, function (err, result) {
+                callback.apply(this, arguments);
+
+                if (!fs.existsSync(cacheDir)) {
+                    fs.mkdirSync(cacheDir);
+                }
+
+                // cache the result
+                fs.writeFile(cacheFilePath, JSON.stringify([result]));
+            }]);
+        }
+    };
+    return connection;
+};
